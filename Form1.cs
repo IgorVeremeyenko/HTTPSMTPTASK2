14dd97d075cb2605507f95dbe0573c647b0aace7
IgorVeremeyenko <72911973+IgorVeremeyenko@users.noreply.github.com>
2021-12-09 14:02:39 +0200

    working

--- Form1.cs
+++ Form1.cs
@@ -19,22 +19,40 @@ namespace HTTPSMTPTASK2
 
         private void Send_Click(object sender, EventArgs e)
         {
-            MailAddress from = new MailAddress(textBoxMessage.Text);
-            MailAddress to = new MailAddress(textBoxRecipient.Text);
-            MailMessage m = new MailMessage(from, to);
-            m.Subject = textBoxTheme.Text;
-            m.Body = textBoxMessage.Text;
-            // адрес smtp-сервера и порт, с которого будем отправлять письмо
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            smtp.Credentials = new NetworkCredential("viruscomp7@gmail.com", "mypassword");
             try
             {
-                smtpClient.Send(mailMessage);
-            }
-            catch (Exception)
-            {
+                MailAddress from = new MailAddress(textBoxSender.Text.Trim());
+                MailAddress to = new MailAddress(textBoxRecipient.Text.Trim());
+                MailMessage m = new MailMessage(from, to);
+                m.Subject = textBoxTheme.Text;
+                m.Body = textBoxMessage.Text;
+                // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential("viruscomp7@gmail.com", "qw79@dsg");
+                smtp.EnableSsl = true;
+                /*smtp.SendAsync(m,"test message1");*/
+                smtp.Send(m);
+                smtp.Timeout = 100;
+                var temp = "Время отправки: меньше " + (smtp.Timeout / 100) + " сек";
+                string message = temp.ToString();
+                string caption = "Письмо отправлено";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
 
-                throw;
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+                this.Close();
+            }
+            catch (Exception msg)
+            {
+                string message = msg.Message;
+                string caption = "Error Detected in Input";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+                
             }
         }
     }
